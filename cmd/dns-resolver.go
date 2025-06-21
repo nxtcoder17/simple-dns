3cbe76aa9bb3a0eade32f7850a6c1b655ad5f603
nxtcoder17 <nxtcoder17@gmail.com>
2025-06-22 01:50:55 +0530

    feat: adds time take to REPLIED log

--- cmd/dns-resolver.go
+++ cmd/dns-resolver.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/miekg/dns"
 )
@@ -30,76 +31,74 @@ func (s *DNSServer) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 
 	logger := logger.With("query.type", dns.Type(q.Qtype).String(), "query.host", query, "request.id", newRequestID())
 
+	start := time.Now()
 	logger.Debug("[REQUEST] started")
-	defer logger.Debug("[REPLIED]")
+	defer func() {
+		logger.Debug("[REPLIED]", "took", fmt.Sprintf("%.2fs", time.Since(start).Seconds()))
+	}()
 
-	switch q.Qtype {
-	case dns.TypeA:
-		{
-			logger.Debug("[step/upstream] dns servers")
-			for k, addr := range cfg.UpstreamDNSServers {
-				if strings.HasSuffix(query, k) {
-					logger.Debug("[step/upstream] found correct upstream", "upstream", k, "@", addr)
-					if !strings.HasSuffix(addr, ":53") {
-						addr += ":53"
-					}
-					reply, err := dns.Exchange(r, addr)
-					if err != nil {
-						logger.Error("[step/upstream] failed to exchange dns with upstream", "err", err, "upstream", k)
-						msg.SetRcode(r, dns.RcodeNameError)
-						w.WriteMsg(msg)
-						return
-					}
-					logger.Debug("[REPLY]", "reply", reply)
-					w.WriteMsg(reply)
-					return
-				}
+	if q.Qtype != dns.TypeA {
+		msg.SetRcode(r, dns.RcodeNotImplemented)
+		w.WriteMsg(msg)
+		return
+	}
+
+	logger.Debug("[step/upstream] dns servers")
+	for k, addr := range cfg.UpstreamDNSServers {
+		if strings.HasSuffix(query, k) {
+			logger.Debug("[step/upstream] found correct upstream", "upstream", k, "@", addr)
+			if !strings.HasSuffix(addr, ":53") {
+				addr += ":53"
 			}
-
-			logger.Debug("[step/simple-dns] inbuilt")
-			ip, err := resolve(query)
+			reply, err := dns.Exchange(r, addr)
 			if err != nil {
-				logger.Debug("[step/simple-dns] failed to resolve", "err", err)
-				msg.SetRcode(r, int(dns.ExtendedErrorCodeStaleNXDOMAINAnswer))
-				rr, _ := dns.NewRR(fmt.Sprintf("%s SOA ", q.Name))
-				msg.Answer = append(msg.Answer, rr)
-				w.WriteMsg(msg)
-				return
-			}
-
-			if ip != "" {
-				logger.Debug("[step/simple-dns] inbuilt found", "ip", ip)
-				rr, err := dns.NewRR(fmt.Sprintf("%s\t5\t%s\t%s\t%s", q.Name, dns.Class(q.Qclass).String(), dns.Type(q.Qtype).String(), ip))
-				if err != nil {
-					msg.SetRcode(r, dns.RcodeFormatError)
-					w.WriteMsg(msg)
-				}
-				msg.Answer = append(msg.Answer, rr)
-				w.WriteMsg(msg)
-				return
-			}
-
-			dnsAddr := cfg.FallbackDNSServers[0]
-			if !strings.HasSuffix(dnsAddr, ":53") {
-				dnsAddr += ":53"
-			}
-			logger.Debug("trying default dns servers", "dns-server", dnsAddr)
-			reply, err := dns.Exchange(r, dnsAddr)
-			if err != nil {
-				logger.Error("[step/fallback] failed to exchange dns with upstream", "err", err, "fallback", dnsAddr)
+				logger.Error("[step/upstream] failed to exchange dns with upstream", "err", err, "upstream", k)
 				msg.SetRcode(r, dns.RcodeNameError)
 				w.WriteMsg(msg)
 				return
 			}
+			logger.Debug("[REPLY]", "reply", reply)
 			w.WriteMsg(reply)
 			return
 		}
-	default:
-		{
-		}
 	}
 
-	// w.WriteMsg(msg)
+	logger.Debug("[step/simple-dns] inbuilt")
+	ip, err := resolve(query)
+	if err != nil {
+		logger.Debug("[step/simple-dns] failed to resolve", "err", err)
+		msg.SetRcode(r, int(dns.ExtendedErrorCodeStaleNXDOMAINAnswer))
+		rr, _ := dns.NewRR(fmt.Sprintf("%s SOA ", q.Name))
+		msg.Answer = append(msg.Answer, rr)
+		w.WriteMsg(msg)
+		return
+	}
+
+	if ip != "" {
+		logger.Debug("[step/simple-dns] inbuilt found", "ip", ip)
+		rr, err := dns.NewRR(fmt.Sprintf("%s\t3600\t%s\t%s\t%s", q.Name, dns.Class(q.Qclass).String(), dns.Type(q.Qtype).String(), ip))
+		if err != nil {
+			msg.SetRcode(r, dns.RcodeFormatError)
+			w.WriteMsg(msg)
+		}
+		msg.Answer = append(msg.Answer, rr)
+		w.WriteMsg(msg)
+		return
+	}
+
+	dnsAddr := cfg.FallbackDNSServers[0]
+	if !strings.HasSuffix(dnsAddr, ":53") {
+		dnsAddr += ":53"
+	}
+	logger.Debug("trying default dns servers", "dns-server", dnsAddr)
+	reply, err := dns.Exchange(r, dnsAddr)
+	if err != nil {
+		logger.Error("[step/fallback] failed to exchange dns with upstream", "err", err, "fallback", dnsAddr)
+		msg.SetRcode(r, dns.RcodeNameError)
+		w.WriteMsg(msg)
+		return
+	}
+	w.WriteMsg(reply)
 }
 
 func resolve(dnsQuery string) (ip string, err error) {
